1a02b32da4d0ef0549ca2a45e44cba4b580d91ff
garrettvercoe <garrettvercoe@gmail.com>
2020-06-17 15:05:15 -0400

    meta fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Garrett Vercoe`,
     description: `Interactive Designer`,
+    author: "Garrett Vercoe",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
